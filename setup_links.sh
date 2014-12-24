59cc4c4698a67171390b791a15299332b278d4df
Geoffrey Anderson <geoff@geoffreyanderson.net>
2014-12-23 19:09:25 -0500

    Simple script to bootstrap symlinks for config files

---
+++ setup_links.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+ln -s "${HOME}/sandbox/dotfiles/gitconfig" "${HOME}/.gitconfig"
+ln -s "${HOME}/sandbox/dotfiles/screenrc" "${HOME}/.screenrc"
+ln -s "${HOME}/sandbox/dotfiles/shell.aliases" "${HOME}/.shell.aliases"
+ln -s "${HOME}/sandbox/dotfiles/tmux.conf" "${HOME}/.tmux.conf"
+ln -s "${HOME}/sandbox/dotfiles/vimrc" "${HOME}/.vimrc"
+ln -s "${HOME}/sandbox/dotfiles/Xdefaults" "${HOME}/.Xdefaults"
+ln -s "${HOME}/sandbox/dotfiles/Xresources" "${HOME}/.Xresources"
+ln -s "${HOME}/sandbox/dotfiles/zshrc" "${HOME}/.zshrc"
+ln -s "${HOME}/sandbox/dotfiles/zshrc.local" "${HOME}/.zshrc.local"
